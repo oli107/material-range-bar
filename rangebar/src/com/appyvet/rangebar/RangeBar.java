c4c4a1024d46e94b95da6ba0b6c9a30675729c2c
Steve Grosbois <steve@playmoweb.com>
2015-12-04 12:07:41 +0100

    Set temporaryPins programmatically

--- rangebar/src/com/appyvet/rangebar/RangeBar.java
+++ rangebar/src/com/appyvet/rangebar/RangeBar.java
@@ -487,7 +487,6 @@ public class RangeBar extends View {
         this.mPinTextListener = mPinTextListener;
     }
 
-
     public void setFormatter(IRangeBarFormatter formatter) {
         if (mLeftThumb != null) {
             mLeftThumb.setFormatter(formatter);
@@ -498,6 +497,18 @@ public class RangeBar extends View {
         }
 
         mFormatter = formatter;
+    }
+
+    public void setArePinsTemporary(boolean mPinsAreTemporary) {
+        if (mLeftThumb != null) {
+            mLeftThumb.setArePinsTemporary(mPinsAreTemporary);
+        }
+
+        if (mRightThumb != null) {
+            mRightThumb.setArePinsTemporary(mPinsAreTemporary);
+        }
+
+        this.mArePinsTemporary = mPinsAreTemporary;
     }
 
     public void setDrawTicks(boolean drawTicks) {
